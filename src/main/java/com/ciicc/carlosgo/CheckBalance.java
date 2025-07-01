abc1c51cb65f54b42be09cd4bc17b7f303934064
Carlos Go <gocarlos519@gmail.com>
2025-07-02 02:06:24 +0800

    Job Sheet 2-2: CheckBalance class. (Refactored)

--- src/main/java/com/ciicc/carlosgo/CheckBalance.java
+++ src/main/java/com/ciicc/carlosgo/CheckBalance.java
@@ -8,21 +8,22 @@ public class CheckBalance {
     private static final String dbPassword = "";
 
     public float checkBalance(int id) {
-
         float balance = 0;
         try (Connection con = con();
-             Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE))
-        {
+             Statement statement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+
             String query = "SELECT * FROM balance WHERE user_ID = " + id;
             ResultSet rs = statement.executeQuery(query);
-            if (rs.next())
+            if (rs.next()) {
                 balance = rs.getFloat("amount");
-            else
-                System.out.println("User not found");
+                System.out.printf("Your current balance is: ₱%,.2f%n", balance);
+            } else {
+                System.out.println("User not found.");
+            }
+
         } catch (SQLException e) {
             System.out.println(e.getLocalizedMessage());
         }
-
         return balance;
     }
 
